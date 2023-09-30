f3fb7f9e3f74f3709d714313d211c511ccdf1dea
bokamix <janiakpol@gmail.com>
2023-09-30 14:02:21 +0200

    New styles

--- src/components/Audio.vue
+++ src/components/Audio.vue
@@ -14,25 +14,29 @@
 
   <div class="container">
     <button
-      @click="clearNote"
-      type="button"
-      class="text-white px-28 bg-gradient-to-r from-red-500 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-    >
-      wyczyść
-    </button>
-    <button
       role="button"
       @click="sendToWebhook"
       type="button"
       class="text-white px-28 bg-gradient-to-br from-green-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
     >
-      Send to webhook
+      <div v-if="sending">
+        <svg aria-hidden="true" role="status" class="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+          </svg>
+          Loading...
+      </div>
+      <div v-else>
+         Send to webhook
+      </div>
     </button>
 
-    <div class="textarea-container">
+    <div class="my-12">
       <textarea
-        class="textarea"
         v-model="note"
+        id="message"
+        rows="10"
+        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Response"
       ></textarea>
     </div>
@@ -48,59 +52,111 @@
               <audio controls playsinline>
                 <source :src="recording.url" type="audio/mp3" />
               </audio>
-              <button @click="sendToWhisper(recording)">Wyślij</button>
+              <button @click="sendToWhisper(recording)" type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Wyślij</button>
             </li>
           </ul>
         </div>
       </div>
     </div>
+  </div>
 
-    <svg v-if="recording" viewBox="0 0 100 100" class="audio-circle-visualizer">
-      <circle
-        v-for="value in audioHistory"
-        :key="value"
-        r="40"
-        cx="50"
-        cy="50"
-        :stroke-dasharray="`${value}, 100`"
-        stroke-width="5"
-        fill="none"
-        :style="{
-          strokeDashoffset: value / 2,
-          transform: `rotate(${-90 + (index * 360) / audioHistory.length}deg)`,
-          stroke: 'white',
-        }"
-      />
-    </svg>
-    <div>
+  <div
+    class="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600"
+  >
+    <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
       <button
+        type="button"
+        @click="clearNote"
+        class="inline-flex flex-col items-center justify-center px-5 rounded-l-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+      >
+        <svg class="w-[19px] h-[19px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+        </svg>
+        <span class="sr-only">Records</span>
+      </button>
+      <div
+        id="tooltip-home"
+        role="tooltip"
+        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+      >
+       Records
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
+      <button
+        data-tooltip-target="tooltip-wallet"
+        type="button"
         @click="toggleModal"
-        type="button"
-        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
       >
-        Nagrania
+        <svg class="w-[21px] h-[21px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 16">
+          <path d="M14.316.051A1 1 0 0 0 13 1v8.473A4.49 4.49 0 0 0 11 9c-2.206 0-4 1.525-4 3.4s1.794 3.4 4 3.4 4-1.526 4-3.4a2.945 2.945 0 0 0-.067-.566c.041-.107.064-.22.067-.334V2.763A2.974 2.974 0 0 1 16 5a1 1 0 0 0 2 0C18 1.322 14.467.1 14.316.051ZM10 3H1a1 1 0 0 1 0-2h9a1 1 0 1 1 0 2Z"/>
+          <path d="M10 7H1a1 1 0 0 1 0-2h9a1 1 0 1 1 0 2Zm-5 4H1a1 1 0 0 1 0-2h4a1 1 0 1 1 0 2Z"/>
+        </svg>
+        <span class="sr-only">Wallet</span>
       </button>
-      <button
-        @click="copy"
-        type="button"
-        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+      <div
+        id="tooltip-wallet"
+        role="tooltip"
+        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
       >
-        copy note
-      </button>
+        Wallet
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
+      <div class="flex items-center justify-center">
+        <button
+          data-tooltip-target="tooltip-new"
+          type="button"
+          @click="() => (recording ? stopRecord() : startRecord())"
+          class="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
+          :class="!recording ? 'pl-1': ''"
+        >
+          <svg v-if="!recording" class="w-[21px] h-[21px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 16">
+            <path d="M0 .984v14.032a1 1 0 0 0 1.506.845l12.006-7.016a.974.974 0 0 0 0-1.69L1.506.139A1 1 0 0 0 0 .984Z"/>
+          </svg>
+          <svg v-if="recording" 
+            class="w-[10px] h-[10px] text-gray-800 dark:text-white" 
+            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 12 16">
+            <path d="M3 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm7 0H9a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Z"/>
+          </svg>
+        </button>
+      </div>
+      <div
+        id="tooltip-new"
+        role="tooltip"
+        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+      >
+        Create new item
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
       <button
+        data-tooltip-target="tooltip-settings"
+        type="button"
         @click="undo"
-        type="button"
-        class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
       >
-        undo
+        <svg class="w-[21px] h-[21px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="m12 7 3-3-3-3m0 12H5.5a4.5 4.5 0 1 1 0-9H14"/>
+        </svg>
+
+        <span class="sr-only">Settings</span>
       </button>
-      <button
-        role="button"
-        @click="() => (recording ? stopRecord() : startRecord())"
-        type="button"
-        class="px-32 py-3.5 text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      <div
+        id="tooltip-settings"
+        role="tooltip"
+        class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
       >
-        {{ recording ? "Stop" : "Record" }}
+        Settings
+        <div class="tooltip-arrow" data-popper-arrow></div>
+      </div>
+      <button
+        type="button"
+        @click="copy"
+        class="inline-flex flex-col items-center justify-center px-5 rounded-r-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+      >
+        <svg class="w-[21px] h-[21px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.8" d="M2 5a1 1 0 0 0-1 1v12a.969.969 0 0 0 .933 1h8.1a1 1 0 0 0 1-1.033M10 1v4a1 1 0 0 1-1 1H5m10-4v12a.97.97 0 0 1-.933 1H5.933A.97.97 0 0 1 5 14V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 9.828 1h4.239A.97.97 0 0 1 15 2Z"/>
+        </svg>
+        <span class="sr-only">copy</span>
       </button>
     </div>
   </div>
@@ -318,135 +374,6 @@ const clearNote = () => {
 </script>
 
 <style scoped lang="scss">
-.container {
-  width: 90%;
-  margin: 0 auto;
-  height: 100vh;
-  position: relative;
-}
-
-.action-button {
-  border: 0;
-  border-radius: 6px;
-  color: #fff;
-  cursor: pointer;
-  display: inline-flex;
-  font-family: "JetBrains Mono", monospace;
-  height: 48px;
-  justify-content: center;
-  overflow: hidden;
-  padding: 16px;
-  background-image: radial-gradient(
-    100% 100% at 100% 0,
-    #5adaff 0,
-    #e47931 100%
-  );
-  margin: 20px 5px;
-  font-size: 18px;
-}
-
-.record-button {
-  width: 250px;
-  background-image: radial-gradient(
-    100% 100% at 100% 0,
-    #5adaff 0,
-    #5468ff 100%
-  );
-  margin: 20px 5px;
-  padding: 16px 32px;
-  align-items: center;
-  font-size: 18px;
-  // ... (reszta styli dla przycisku)
-  &--stop {
-    background-image: radial-gradient(100% 100% at 100% 0, red 0, pink 100%);
-  }
-}
-
-.textarea {
-  width: 96%;
-  min-height: 250px;
-  box-sizing: border-box;
-  border: none;
-  background-color: transparent;
-  font-size: 16px;
-  color: white;
-  resize: none;
-}
-
-.send-button {
-  background-color: #4caf50;
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: center;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-
-.record-button-wrapper {
-  position: absolute;
-  padding: 16px 32px;
-  width: 80vw;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: 50px;
-}
-.loading-spinner {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5); // półprzezroczyste tło
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000; // aby upewnić się, że jest na wierzchu
-}
-
-.spinner {
-  border: 16px solid #f3f3f3;
-  border-top: 16px solid #3498db; // Niebieski kolor
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  animation: spin 1s linear infinite;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-.audio-circle-visualizer {
-  width: 200px;
-  height: 200px;
-  margin: 20px auto;
-
-  circle {
-    transition: stroke-dashoffset 0.05s;
-  }
-}
-.recordings-list {
-  // ... istniejące style
-
-  button {
-    background-color: #4caf50;
-    border: none;
-    color: white;
-    padding: 5px 10px;
-    margin-left: 10px;
-    cursor: pointer;
-    border-radius: 4px;
-    &:hover {
-      background-color: #45a049;
-    }
-  }
-}
 .open-modal-button {
   background-color: #ff7f00;
   border: none;
